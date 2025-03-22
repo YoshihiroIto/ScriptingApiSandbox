ed353c2326ec8b6b5795dcb93fe331a8266f8229
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2025-03-22 09:54:05 +0900

    feat: Update project structure and add Avalonia specific files with dependency management
    - Update .gitignore to include specific directories for better project management.
    - Add new Visual Studio solution file ScriptingApiSandbox.sln
    - Add new Avalonia application configuration file for ScriptingApiSandbox
    - Add new App class for the application lifecycle and main window initialization, disable Avalonia's data annotation validation.
    - Add avalonia logo icon file
    - Add new MainWindow XAML file with data binding for greeting text
    - Add new file `MainWindow.axaml.cs` for the main window implementation
    - Add MainWindowViewModel for the new UI in ScriptingApiSandbox.
    - Add new main entry point for Avalonia application in Program.cs
    - Add new project file for net9.0 Avalonia application with specific dependencies and configurations.
    - Add Windows compatibility support for application manifest
    - Add new configuration settings for XamlStyler.

---
+++ ScriptingApiSandbox/MainWindow/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ScriptingApiSandbox.MainWindow;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ScriptingApiSandbox/MainWindow/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ScriptingApiSandbox.MainWindow;
+
+public sealed partial class MainWindowViewModel : ObservableObject
+{
+    [ObservableProperty]
+    public partial string Greeting { get; set; } = "Welcome to Avalonia!";
+}
