1b87ea72ef13d7930e4d056caf721a5879cb824b
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2025-03-22 21:15:19 +0900

    feat: Added ScriptContext class, custom output for Python script execution, improved UI layout using DockPanel and StackPanel.
    - Add ScriptContext class and handle custom output for Python script execution.
    - Add IronPython package references to ScriptingApi project file.
    - Remove Visual Studio solution file
    - Add new code style settings for C# in .DotSettings file
    - Add projects to ScriptingApiSandbox solution.
    - Change UI layout to use a DockPanel and StackPanel for better organization.
    - Add support for script execution and interaction with sample object in MainWindowViewModel
    - Add ReactiveProperty package reference and project reference to ScriptingApiSandbox.csproj

---
+++ ScriptingApi/ScriptContext.cs
@@ -0,0 +1,137 @@
+﻿using IronPython.Hosting;
+using IronPython.Runtime.Types;
+using Microsoft.Scripting.Hosting;
+
+namespace ScriptingApi;
+
+public sealed class ScriptContext
+{
+    public IEnumerable<string> AllFunctionNames
+    {
+        get
+        {
+            foreach (var item in _scope.GetItems())
+                if (item.Value is IronPython.Runtime.PythonFunction)
+                    yield return item.Key;
+        }
+    }
+
+    public event EventHandler<StandardOutputEventArgs>? StandardOutput;
+
+    private readonly ScriptEngine _pythonEngine = Python.CreateEngine();
+    private readonly ScriptScope _scope;
+
+    public ScriptContext()
+    {
+        _scope = CreateScope();
+    }
+
+    public void SetVariable(string name, object? obj)
+    {
+        _scope.SetVariable(name, obj);
+    }
+
+    public void SetType<T>(string name)
+    {
+        SetType(name, typeof(T));
+    }
+
+    public void SetType(string name, Type type)
+    {
+        _scope.SetVariable(name, DynamicHelpers.GetPythonTypeFromType(type));
+    }
+
+    public dynamic GetVariable(string name)
+    {
+        return _scope.GetVariable(name);
+    }
+
+    public void Execute(string code)
+    {
+        try
+        {
+#if false
+            var source = _pythonEngine.CreateScriptSourceFromString(code, Microsoft.Scripting.SourceCodeKind.Statements);
+            var compiled = source.Compile();
+            compiled.Execute(_scope);
+#else
+            _pythonEngine.Execute(code, _scope);
+#endif
+
+        }
+        catch (Exception ex)
+        {
+            InvokeStandardOutput($"エラー: {ex.Message}");
+            InvokeStandardOutput($"例外の種類: {ex.GetType().FullName}");
+            InvokeStandardOutput($"スタックトレース: {ex.StackTrace}");
+        }
+    }
+
+    public object? CallFunction(string functionName)
+    {
+        return CallFunctionInternal(() => _scope.GetVariable(functionName)());
+    }
+
+    public object? CallFunction<T0>(string functionName, T0 arg0)
+    {
+        return CallFunctionInternal(() => _scope.GetVariable(functionName)(arg0));
+    }
+
+    public object? CallFunction<T0, T1>(string functionName, T0 arg0, T1 arg1)
+    {
+        return CallFunctionInternal(() => _scope.GetVariable(functionName)(arg0, arg1));
+    }
+
+    public void InvokeStandardOutput(string output)
+    {
+        StandardOutput?.Invoke(this, new StandardOutputEventArgs(output));
+    }
+
+    private ScriptScope CreateScope()
+    {
+        var scope = _pythonEngine.CreateScope();
+        scope.SetVariable("__custom_print__", new Action<string>(InvokeStandardOutput));
+
+        try
+        {
+            _pythonEngine.Execute("""
+                                  import sys
+
+                                  class CustomOutput:
+                                      def write(self, text):
+                                          if text and text.strip():
+                                              __custom_print__(text)  # C#関数を呼び出し
+                                      def flush(self):
+                                          pass
+
+                                  sys.stdout = CustomOutput()
+                                  sys.stderr = CustomOutput()
+                                  """,
+                scope);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"初期化エラー: {ex.Message}");
+        }
+
+        return scope;
+    }
+
+    private static object? CallFunctionInternal(Func<object?> func)
+    {
+        try
+        {
+            return func();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error calling function: {ex.Message}");
+            return null;
+        }
+    }
+}
+
+public class StandardOutputEventArgs(string text) : EventArgs
+{
+    public readonly string Text = text;
+}
--- ScriptingApiSandbox/MainWindow/MainWindowViewModel.cs
+++ ScriptingApiSandbox/MainWindow/MainWindowViewModel.cs
@@ -1,9 +1,147 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Reactive.Bindings.Extensions;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Reactive.Bindings;
+using ScriptingApi;
 
 namespace ScriptingApiSandbox.MainWindow;
 
 public sealed partial class MainWindowViewModel : ObservableObject
 {
     [ObservableProperty]
-    public partial string Greeting { get; set; } = "Welcome to Avalonia!";
+    public partial string Script { get; set; } = """
+                                                 print(sample.Name)
+                                                 sample.Name = 'IronPython'
+                                                 sample.SayHello()
+
+                                                 print(StaticMethod())
+                                                 print(StaticClass.StaticMethod())
+                                                 print(Color.Red)
+
+                                                 print(Math.Abs(-123))
+                                                 print(sample.Color == Color.Green)
+                                                 print(sample.Color)
+                                                 print(type(Color))
+
+                                                 notifier = Notifier()
+
+                                                 # イベントハンドラを定義して購読
+                                                 def on_notify(sender, args):
+                                                     print("Event received!")
+
+                                                 notifier.OnNotify += on_notify
+
+                                                 # イベントを発火
+                                                 notifier.Notify()  # "Notify called!" と "Event received!" が表示される
+
+                                                 def add(a, b):
+                                                     return a + b
+
+                                                 def greet(name):
+                                                     return f'Hello, {name}!'
+                                                 """;
+
+
+    [ObservableProperty]
+    public partial string AllFunctions { get; set; } = "";
+
+    public ReadOnlyReactiveCollection<string> Stdout { get; }
+
+    private readonly ScriptContext _scriptContext = new();
+    private readonly ReactiveCollection<string> _stdout;
+    private readonly CompositeDisposable _trash = new();
+
+    public MainWindowViewModel()
+    {
+        _stdout = new ReactiveCollection<string>().AddTo(_trash);
+        Stdout = _stdout.ToReadOnlyReactiveCollection().AddTo(_trash);
+
+        Observable.FromEventPattern<EventHandler<StandardOutputEventArgs>, StandardOutputEventArgs>(
+                h => h,
+                h => _scriptContext.StandardOutput += h,
+                h => _scriptContext.StandardOutput -= h)
+            .Select(e => e.EventArgs)
+            .Subscribe(e =>
+            {
+                _stdout.Add(e.Text);
+            })
+            .AddTo(_trash);
+
+
+        var sample = new SampleObject(_scriptContext);
+        _scriptContext.SetVariable("sample", sample);
+        _scriptContext.SetVariable("StaticMethod", new Func<string>(StaticClass.StaticMethod));
+        _scriptContext.SetType(nameof(StaticClass), typeof(StaticClass));
+        _scriptContext.SetType<Color>(nameof(Color));
+        _scriptContext.SetType(nameof(Math), typeof(Math));
+        _scriptContext.SetType<Notifier>(nameof(Notifier));
+
+        ExecuteScript();
+    }
+
+    [RelayCommand]
+    private void ExecuteScript()
+    {
+        _stdout.Clear();
+        _scriptContext.Execute(Script);
+
+        AllFunctions = string.Join("\n", _scriptContext.AllFunctionNames);
+    }
+    
+    [RelayCommand]
+    private void Add()
+    {
+        var result = _scriptContext.CallFunction("add", 10, 20);
+        
+        Console.WriteLine(result);
+    }
+    
+    [RelayCommand]
+    private void Greet()
+    {
+        var result = _scriptContext.CallFunction("greet", "Yoshihiro");
+        
+        Console.WriteLine(result);
+    }
+}
+
+public enum Color
+{
+    Red,
+    Green,
+    Blue
+}
+
+public static class StaticClass
+{
+    public static string StaticMethod()
+    {
+        return "This is a static method.";
+    }
+}
+
+public class SampleObject(ScriptContext scriptContext)
+{
+    public string Name { get; set; } = "DefaultName";
+
+    public Color Color { get; set; } = Color.Green;
+
+    public void SayHello()
+    {
+        scriptContext.InvokeStandardOutput($"Hello from {Name}!");
+    }
+}
+
+public class Notifier
+{
+    public event EventHandler? OnNotify;
+
+    public void Notify()
+    {
+        Console.WriteLine("Notify called!");
+        OnNotify?.Invoke(this, EventArgs.Empty);
+    }
 }
